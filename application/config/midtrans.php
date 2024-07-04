e8bfe9a4402da9cbca09dc326197c564db5aa65b
desnathasatria <satriadesnatha@gmail.com>
2024-07-04 20:22:31 +0700

    midtrans

--- application/config/midtrans.php
+++ application/config/midtrans.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['midtrans'] = array(
-    'server_key' => 'SB-Mid-server-pQiRQ1mnSISaRDW3NpHkoVn_',  // Ganti dengan server key Anda
+    'server_key' => 'SB-Mid-server-bwXF1tNUFjYvXoseoKNPperM',  // Ganti dengan server key Anda
     'is_production' => false,  // Ubah ke true jika menggunakan environment production
-    'client_key' => 'SB-Mid-client-tcx9_40nrUkNZEOZ',  // Ganti dengan client key Anda (jika diperlukan)
+    'client_key' => 'SB-Mid-client-9O2npFLxwIFtEqMm',  // Ganti dengan client key Anda (jika diperlukan)
 );
