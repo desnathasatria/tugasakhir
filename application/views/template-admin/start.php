f8aa89f2d507cfedc8c0004c22fa156c71154831
JihanCahya <jihan69cahya@gmail.com>
2024-07-15 11:48:42 +0700

    perbaikan galeri user

--- application/views/template-admin/start.php
+++ application/views/template-admin/start.php
@@ -9,26 +9,17 @@
         <?= $title ?>
     </title>
 
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/dist/css/adminlte.min.css">
-    <link rel="stylesheet"
-        href="<?= base_url() ?>assets/template-admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet"
-        href="<?= base_url() ?>assets/template-admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-    <link rel="stylesheet"
-        href="<?= base_url() ?>assets/template-admin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
-    <link rel="stylesheet"
-        href="<?= base_url() ?>assets/template-admin/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/select2/css/select2.min.css">
-    <link rel="stylesheet"
-        href="<?= base_url() ?>assets/template-admin/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/daterangepicker/daterangepicker.css">
-    <link rel="stylesheet"
-        href="<?= base_url() ?>assets/template-admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template-admin/plugins/tempusdominus-bootstrap-/css/tempusdominus-bootstrap-4.min.css">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
--- assets/template-user/js/gallery_filter.js
+++ assets/template-user/js/gallery_filter.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-	// Fungsi untuk mengecek dan menampilkan pesan jika tidak ada foto
 	function checkAndDisplayNoPhotoMessage() {
 		const items = $(".item-content").parent();
 		let hasVisibleItems = false;
@@ -8,6 +7,7 @@ $(document).ready(function () {
 			const item = $(this);
 			if (!item.hasClass("d-none")) {
 				hasVisibleItems = true;
+				return false;
 			}
 		});
 
@@ -18,28 +18,30 @@ $(document).ready(function () {
 		}
 	}
 
-	// Panggil fungsi untuk mengecek dan menampilkan pesan saat halaman dimuat
 	checkAndDisplayNoPhotoMessage();
 
 	$(".filter-button").on("click", (e) => {
 		const filter = $(e.target).attr("data-filter");
-		console.log(filter);
 		const items = $(".item-content").parent();
 		let hasVisibleItems = false;
 
 		items.each(function () {
 			const item = $(this);
-			if (filter == "" || item.attr("data-category") == filter) {
-				item.addClass("animated zoomIn faster");
-				item.removeClass("d-none");
+
+			if (filter == "" || $(item).attr("data-category") == filter) {
+				$(item).addClass("animated zoomIn faster");
+				$(item).parent().addClass("animated zoomIn faster");
+				$(item).removeClass("d-none");
+				$(item).parent().removeClass("d-none");
 				hasVisibleItems = true;
 			} else {
-				item.addClass("d-none");
-				item.removeClass("animated zoomIn faster");
+				$(item).addClass("d-none");
+				$(item).parent().addClass("d-none");
+				$(item).removeClass("animated zoomIn faster");
+				$(item).parent().removeClass("animated zoomIn faster");
 			}
 		});
 
-		// Menampilkan teks "tidak ada foto" jika tidak ada item yang ditampilkan
 		if (!hasVisibleItems) {
 			$(".no-photo-message").removeClass("d-none");
 		} else {
